95d6f7fb0a194d2a55052213a323cd0dc9cb850e
pedroleandrog <pedro.leandrog@gmail.com>
2020-05-29 18:24:01 -0300

    Condições - Parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 80
+
+console.log(`A velocidade do seu carro é de ${velocidade}`)
+
+if(velocidade > 60){ //Condição Simples
+    console.log("Você ultrapassou a velocidade permitida. Multado!")
+}
+
+console.log("Dirija usando centro de segurança!")
