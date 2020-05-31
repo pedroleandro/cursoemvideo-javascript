044495fc9ce88f77099263e3d71a8d628a47624e
pedroleandrog <pedro.leandrog@gmail.com>
2020-05-30 22:41:40 -0300

    Exercício Tabuada

--- aula14ex/ex017/assets/js/script.js
+++ aula14ex/ex017/assets/js/script.js
@@ -1,1 +1,19 @@
-window.alert("Olá")
+function criarTabuada(){
+    let numero = window.document.getElementById(`txtnumero`)
+    let tabuada = window.document.getElementById(`tabuada`)
+
+    if(numero.value == ''){
+        window.alert("[ERROR 404!] Verifique os dados e tente novamente!")
+    }else{
+        let n = Number(numero.value)
+        let c = 1
+        tabuada.innerHTML = ``
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `item${c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
+}
